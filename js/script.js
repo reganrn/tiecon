4eb46ff5f772cc4fe1e59790ebbfdb11068ef01f
regan <reganbca@gmail.com>
2020-09-06 22:08:31 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -17,14 +17,30 @@ $(function(){
         menuFun()
     });
 
+    // Scroll menu active
     $('.navbar-nav li').click(function(){
-        $('.navbar-nav li a').removeClass('active');
-        $(this).find('a').addClass('active');
+        if($('.navbar-collapse').hasClass('show')){
+            $('.navbar-collapse').removeClass('show');
+        }
+        $('.navbar-nav li').removeClass('active');
+        $(this).addClass('active');
         $('html, body').animate({scrollTop : $(".tiecon-sec:eq('" + $(this).index() + "')").offset().top - 60});
     });
-    
-    $window.scroll(function() {
-        
-    });
+
+    $(window).scroll(function() {
+    var windscroll = $(window).scrollTop();
+    if (windscroll >= 100) {
+        $('.tiecon-sec').each(function(i) {
+            if ($(this).position().top <= windscroll + 100) {
+                $('.navbar-nav li.active').removeClass('active');
+                $('.navbar-nav li').eq(i).addClass('active');
+            }
+        });
+
+    } else {
+        $('.navbar-nav li.active').removeClass('active');
+    }
+    }).scroll();
+
 
 })
